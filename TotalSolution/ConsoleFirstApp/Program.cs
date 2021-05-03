b92b3c98cd9fe6775ffa0f71c073d559db283e7f
bigklopp <zmxncbv4@naver.com>
2021-05-03 11:12:28 +0900

    클래스 복습했음..

--- TotalSolution/ConsoleFirstApp/Program.cs
+++ TotalSolution/ConsoleFirstApp/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleFirstApp
 {
+    class Student
+    {
+        public string name;
+        public int grade;
+
+        public static void kkk()
+        {
+            Console.WriteLine("안녕하세요");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); //주석
             Console.WriteLine("Second print");
+
+            Student aStudent = new Student() { grade = 1, name = "연습용" };
+            Student aStudent1 = new Student();
+            aStudent1.grade = 1;
+
+            List<Student> list = new List<Student>();
+            list.Add(new Student() { name = "아아", grade = 1 });
+            list.Add(aStudent);
+            Student.kkk();
         }
     }
 }
